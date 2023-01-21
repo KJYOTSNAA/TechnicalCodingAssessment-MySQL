723c93385fa15cc956459f047355d0789a63c2b8
K. JYOTSNA <83228398+KJYOTSNAA@users.noreply.github.com>
2023-01-21 18:00:06 +0530

    Update Problem1.sql

--- Problem1.sql
+++ Problem1.sql
@@ -1,10 +1,9 @@
 Write a SQL statement to create a table named countries including columns country_id, country_name and region_id and
 make sure that no countries except Italy, India and China will be entered in the table.
 Solu:
-
 CREATE TABLE IF NOT EXISTS countries ( 
-COUNTRYID varchar(5),
-COUNTRYNAME varchar(50)
-CHECK(COUNTRYNAME IN('Italy','India','China')) ,
-REGIONID decimal(10,0)
+COUNTRY_ID varchar(2),
+COUNTRY_NAME varchar(40)
+CHECK(COUNTRY_NAME IN('Italy','India','China')) ,
+REGION_ID decimal(10,0)
 );
